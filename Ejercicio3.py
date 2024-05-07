3e0b229566ce0ebced0abc3dc6898c6ab3a589b6
BurciagaGovea <rodrigovea0@gmail.com>
2024-05-07 09:43:54 -0600

    gg

---
+++ Ejercicio3.py
@@ -0,0 +1,6 @@
+print("Ingrese la base del triángulo")
+b = float(input())
+print("Ingrese la altura del triángulo")
+h = float(input())
+a = b * h / 2
+print("El área del triángul es: ", a)
---
+++ Ejercicio4.py
@@ -0,0 +1,5 @@
+import math
+print("Ingrese el diámetro del círculo")
+D = float(input())
+a = ((D**2)*math.pi)/4
+print("El área del círculo es: ", a)
